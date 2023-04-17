8a7448f407d55f15f2d816d2d7d8a2a3491b0bad
Julka Strasser <julia.strasser@dhi.com.pl>
2023-04-17 11:18:10 +0200

    Less messy playback

--- GamePlayback.py
+++ GamePlayback.py
@@ -8,35 +8,23 @@ import matplotlib.pyplot as plt
 
 class PlayBack():
     def __init__(self):
-       pass
+       self.output_file = "history.avi"
         
-
     def save_game(self):
         window_name = "python3"
         fourcc = cv2.VideoWriter_fourcc(*"XVID")
-        
         self.fps = 12.0 # frames per second
-        # the time you want to record in seconds
-        self.record_seconds = 100 # to bede chciala zamienic na ponowne klikniecie przycisku PlayBack
+        # self.record_seconds = 100 # to bede chciala zamienic na ponowne klikniecie przycisku PlayBack
         self.w = gw.getWindowsWithTitle(window_name)[0]
-        # activate the window
         self.w.activate()
-        # create the video write object
-        self.out = cv2.VideoWriter("history.avi", fourcc, self.fps, tuple(self.w.size))
-        # for i in range(int(self.record_seconds * self.fps)):
+        self.out = cv2.VideoWriter(self.output_file, fourcc, self.fps, tuple(self.w.size))
         print('Zapisywanie historii')
         while True:
             
-            # make a screenshot
             img = pyautogui.screenshot(region=(self.w.left, self.w.top, self.w.width, self.w.height))
-            # convert these pixels to a proper numpy array to work with OpenCV
             frame = np.array(img)
-            # convert colors from BGR to RGB
             frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-            # write the frame
             self.out.write(frame)
-            # show the frame
-            #cv2.imshow("screenshot", frame)
             # if the user clicks q, it exits
             if cv2.waitKey(1) == ord("q"):
                 print('Przerwano zapisywanie historii')
@@ -44,8 +32,7 @@ class PlayBack():
 
     def display_history(self):
 
-        # Create a VideoCapture object and read from input file
-        cap = cv2.VideoCapture('history.avi')
+        cap = cv2.VideoCapture(self.output_file)
 
         # Check if camera opened successfully
         if (cap.isOpened()== False):
@@ -54,10 +41,8 @@ class PlayBack():
         # Read until video is completed
         while(cap.isOpened()):
             
-        # Capture frame-by-frame
             ret, frame = cap.read()
             if ret == True:
-            # Display the resulting frame
                 cv2.imshow('Frame', frame)
                 
             # Press Q on keyboard to exit
@@ -67,14 +52,11 @@ class PlayBack():
         # Break the loop
             else:
                 break
-        # When everything done, release
-        # the video capture object
+        
         cap.release()
-        # Closes all the frames
         cv2.destroyAllWindows()
 
     def stop(self):
-        # make sure everything is closed when exited
         cv2.destroyAllWindows()
         self.out.release()
 
