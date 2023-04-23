09b0dcfae0de9873ae199a8a9c202229a7a7a14f
Julka Strasser <julia.strasser@dhi.com.pl>
2023-04-23 14:03:22 +0200

    Working ClientClass.py

---
+++ ClientClass.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+
+#czyli Gracz 1 xD
+import socket, time
+import json
+
+class Client:
+    def __init__(self,host_ip, port):
+        self.host_ip = host_ip
+        self.port = port
+
+    def read_json_file(self,file_name):
+        f = open(file_name)
+        # returns JSON object as 
+        # a dictionary
+        data = str(json.load(f))
+        print(data)
+        return data
+
+    def Tcp_connect(self):
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.s.connect((self.host_ip, self.port))
+        return
+    
+    def Tcp_Write(self,D):
+        mes = str(D + '\r')
+        self.s.send(mes.encode())
+        return 
+        
+    def Tcp_Read(self):
+        a = ' '
+        b = ''
+        while a != '\r':
+            a = self.s.recv(1).decode()
+            b = b + a
+        return b
+
+    def Tcp_Close(self):
+        self.s.close()
+        return 
+   
+client = Client('192.168.56.1', 17098)
+data = client.read_json_file('GameBoard.json')
+client.Tcp_connect()
+
+client.Tcp_Write(data)
+print(client.Tcp_Read())
+client.Tcp_Write('server')
+print(client.Tcp_Read())
+
+client.Tcp_Close()
--- Test/testClient.py
+++ Test/testClient.py
@@ -26,26 +26,7 @@ def Tcp_Close( ):
    s.close()
    return 
    
-
-
-# # a Python object (dict):
-# x = {"board": []}
-# cell = {"cell_0_0": {}}
-# tile = {"empty": False, "color": "blue", "number": 3}
-# cell["cell_0_0"] = tile
-
-# cell1 = {"cell_0_1": {}}
-# tile = {"empty": False, "color": "blue", "number": 1}
-# cell1["cell_0_1"] = tile
-
-# # cell["cell_0_0"] = tile
-# x["board"].append(cell)
-# x["board"].append(cell1)
-# # convert into JSON:
-# y = json.dumps(x)
-
 f = open('GameBoard.json')
-  
 # returns JSON object as 
 # a dictionary
 data = str(json.load(f))
